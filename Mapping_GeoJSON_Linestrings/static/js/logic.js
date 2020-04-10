b8e4acaa637f1d802b20f6ecf2ff554ccd5aec34
mattglibota <59847215+mattglibota@users.noreply.github.com>
2020-04-09 20:42:11 -0700

    upload json

--- Mapping_GeoJSON_Linestrings/static/js/logic.js
+++ Mapping_GeoJSON_Linestrings/static/js/logic.js
@@ -24,7 +24,7 @@ let baseMaps = {
 let map = L.map('mapid', {
 	center: [44.0, -80.0],
 	zoom: 2,
-	layers: [streets]
+	layers: [light]
 });
 
 // Pass our map layers into our layers control and add the layers control to the map.
